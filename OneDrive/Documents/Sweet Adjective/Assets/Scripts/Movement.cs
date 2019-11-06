6110a3460462b073a63558822ec6ead7c7e59074
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-11-05 21:29:23 -0500

    beginning movements

---
+++ OneDrive/Documents/Sweet Adjective/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get verical and horizontal axis
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        Vector2 position = transform.position;
+        //horizontal movements
+        position.x = position.x + 0.3f * horizontal;
+        //vertical movements
+        position.y = position.y + 0.3f * vertical;
+
+        transform.position = position;
+    }
+}
